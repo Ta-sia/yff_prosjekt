dd34eea2d7b9aa269c94c272e42ec09e05cde9f9
Aron Rønneberg <aron.roenneberg@gmail.com>
2025-04-28 14:33:07 +0200

    Bra start

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from flask import Flask, render_template, redirect, request
-import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
